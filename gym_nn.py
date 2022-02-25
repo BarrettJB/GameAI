38cf580694b92ee3a84be62e51286eb3722aa5cd
Barrett Bryson <bryson.barrett@gmail.com>
2022-02-25 16:12:41 -0500

    Finished following part 2
    However my code doesn't work properly, added copy of example for debug

--- gym_nn.py
+++ gym_nn.py
@@ -8,20 +8,32 @@ Created on Thu Feb 24 20:32:05 2022
 #example taken from https://towardsdatascience.com/creating-deep-neural-networks-from-scratch-an-introduction-to-reinforcement-learning-part-i-549ef7b149d2
 #https://towardsdatascience.com/creating-deep-neural-networks-from-scratch-an-introduction-to-reinforcement-learning-6bba874019db
 
+#further resources
+#http://www.briandolhansky.com/blog/2013/9/27/artificial-neural-networks-backpropagation-part-4
+
 import gym
 import numpy as np
 from collections import deque
+
+def relu(mat):
+  return np.multiply(mat,(mat>0))
+
+def relu_derivative(mat):
+  return (mat>0)*1
 
 class NNLayer:
   def __init__(self, input_size, output_size, activation=None, lr = 0.001):
     self.input_size = input_size
     self.output_size = output_size
     self.weights = np.random.uniform(low=-0.5, high=0.5, size=(input_size, output_size))
-    self.activation_function = activation()
+    self.activation_function = activation
     self.lr = lr
-    
+
+  def update_weights(self, gradient):
+    self.weights = self.weights = self.lr*gradient  
+  
   def forward(self, inputs, remember_for_backprop=True):
-    input_with_bias = np.append(np.ones((len(inputs),1)),inputs, axis=1)
+    input_with_bias = np.append(inputs,1)
     unactivated = np.dot(input_with_bias,self.weights)
     output = unactivated
     if self.activation_function != None:
@@ -29,8 +41,16 @@ class NNLayer:
     if remember_for_backprop:
       self.backward_store_in = input_with_bias
       self.backward_store_out = np.copy(unactivated)
-      
     return output
+      
+  def backward(self, gradient):
+    adjusted_mul = gradient
+    if self.activation_function != None:
+      adjusted_mul = np.multiply(relu_derivative(self.backward_store_out), gradient)
+    D_i = np.dot(np.transpose(np.reshape(self.backward_store_in,(1,len(self.backward_store_in)))), np.reshape(adjusted_mul,(1,len(adjusted_mul))))
+    delta_i = np.dot(adjusted_mul, np.transpose(self.weights))[:-1]
+    self.update_weights(D_i)
+    return delta_i
 
 class RLAgent:
   env = None
@@ -43,14 +63,15 @@ class RLAgent:
     self.num_hidden_layers = 2
     self.epsilon = 1.0
     self.memory = deque([],1000000)
+    self.gamma = 0.95
     
-    self.layers = [NNLayer(self.input_size+1,self.hidden_size,activation=self.relu)]
+    self.layers = [NNLayer(self.input_size+1,self.hidden_size,activation=relu)]
     for i in range(self.num_hidden_layers-1):
-      self.layers.append(NNLayer(self.hidden_size+1, self.hidden_size, activation=self.relu))
+      self.layers.append(NNLayer(self.hidden_size+1, self.hidden_size, activation=relu))
     self.layers.append(NNLayer(self.hidden_size+1,self.output_size))
     
   def select_action(self, observation):
-    values = self.foward(observation)
+    values = self.forward(observation)
     if (np.random.random() > self.epsilon):
       return np.argmax(values)
     else:
@@ -64,8 +85,10 @@ class RLAgent:
       index += 1
     return vals
   
-  def relu(mat):
-    return np.multiply(mat,(mat>0))
+  def backward(self, calculated_values, experimental_values):
+    delta = (calculated_values - experimental_values)
+    for layer in reversed(self.layers):
+      delta = layer.backward(delta)
   
   def remember(self, done, action, observation, prev_obs):
     self.memory.append([done, action, observation, prev_obs])
@@ -85,7 +108,7 @@ class RLAgent:
         else:
           experimental_values[action_selected] = 1 + self.gamma*np.max(next_action_values)
         self.backward(action_values, experimental_values)
-        self.epsilon = self.epsilon if self.epsilon < 0.01 else self.epsilon*0.995
+        self.epsilon = self.epsilon if self.epsilon < 0.01 else self.epsilon*0.999
         for layer in self.layers:
           layer.lr - layer.lr if layer.lr < 0.0001 else layer.lr*0.995
         
@@ -93,10 +116,12 @@ class RLAgent:
 
 if __name__ == '__main__':
   env = gym.make('CartPole-v1')
-  NUM_EPISODES = 10
+  NUM_EPISODES = 5000
   MAX_TIMESTEPS = 1000
   model = RLAgent(env)
   
+  perf_tracker = np.zeros(25)
+
   for i in range(NUM_EPISODES):
     observation = env.reset()
     for t in range(MAX_TIMESTEPS):
@@ -106,7 +131,7 @@ if __name__ == '__main__':
       observation, reward, done, info = env.step(action)
       model.remember(done, action, observation, prev_obs)
       model.experience_replay(20)
-      model.epsilon = model.epsilon if model.epsilon < 0.01 else model.epsilon*0.995
       if done:
-        print('Episode {} ended after {} timesteps, current exploration is {}'.format(i_episode, t+1,model.epsilon))
+        perf_tracker[i%25] = t+1
+        print('Episode {} ended after {} timesteps, current exploration is {}, avg run is {}'.format(i, t+1,model.epsilon,np.average(perf_tracker)))
         break
